de0ed24e7aa0315cc84cd82852ab197a26fe6907
Bruce Yang <Bruce.Yang@kla-tencor.com>
2020-05-29 14:15:34 +0800

    Init demo project for xslt (support debug).

---
+++ xsltDebugDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml.Xsl;
+
+namespace xsltDebugDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create a new XslCompiledTransform and load the compiled transformation.
+            XslCompiledTransform xslt = new XslCompiledTransform(true);  // Enable XSLT debugging.
+
+            // string basePath = Directory.GetCurrentDirectory();
+            string basePath = Path.GetDirectoryName(@"../../../");
+            var transformFilePath = Path.Combine(basePath, @"Transform.xsl");
+            string outputFile = @"below-average.xml";
+            
+            try
+            {
+                xslt.Load(transformFilePath);
+
+                // Execute the transformation and output the results to a file.
+                // xslt.Transform(Path.Combine(basePath, @"books.xml"), "discount_books.html");
+                FileStream outputStream = new FileStream(outputFile, FileMode.Append);
+                xslt.Transform(Path.Combine(basePath, @"books.xml"), null, outputStream);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ xsltDebugDemo/Transform.xsl
@@ -0,0 +1,18 @@
+﻿<?xml version='1.0'?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:output method="xml" encoding="utf-8"/>
+  <xsl:template match="/">
+    <xsl:variable name="bookCount" select="count(/bookstore/book)"/>
+    <xsl:variable name="bookTotal" select="sum(/bookstore/book/price)"/>
+    <xsl:variable name="bookAverage" select="$bookTotal div $bookCount"/>
+    <books>
+      <!--Books That Cost Below Average-->
+      <xsl:for-each select="/bookstore/book">
+        <xsl:if test="price &lt; $bookAverage">
+          <xsl:copy-of select="."/>
+        </xsl:if>
+      </xsl:for-each>
+    </books>
+  </xsl:template>
+</xsl:stylesheet>
